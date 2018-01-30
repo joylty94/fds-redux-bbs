59e0f518f3458052999a3e12201e3a3dfd677779
joylty94 <wowmen1757@gmail.com>
2018-01-30 14:46:53 +0900

    로그인 화면

---
+++ src/components/LoginScreen.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import {
+  Button,
+  Icon,
+  Grid,
+  Segment,
+  Header
+} from 'semantic-ui-react'
+import styled from 'styled-components'
+
+// Sementic UI(Grid, Button, Header, Segment)
+
+const FullHeightGrid = styled(Grid)`
+  height: 100vh;
+`
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <FullHeightGrid centered verticalAlign="middle">
+        <Grid.Column style={{width: '280px'}}>
+          <Segment stacked textAlign="center" padded = "very">
+            <Header as='h1'>로그인</Header>
+            <Button color='google plus' fluid>
+              <Icon name='google plus' /> 구글로 로그인
+            </Button>
+            <Button color='twitter' fluid style={{marginTop: '10px'}}>
+              <Icon name='twitter' /> 트위터로 로그인
+            </Button>
+          </Segment>
+        </Grid.Column>
+      </FullHeightGrid>
+    )
+  }
+}

e5df7dbf60ba911a54c60b41410700f3cb443981
joylty94 <wowmen1757@gmail.com>
2018-02-01 12:33:23 +0900

    ArticleList 완성

--- stories/ArticleList.js
+++ stories/ArticleList.js
@@ -3,7 +3,33 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import ArticleList from '../src/components/ArticleList';
 
+const articles = [
+  {
+    id: 'article0',
+    title: '게시글 제목',
+    createdAt: 1517453824996,
+    nickName: '이태용',
+  },
+  {
+    id: 'article1',
+    title: '게시글 제목2',
+    createdAt: 1517453824996,
+    nickName: '이태용',
+  },
+];
+
+const articlesWithLink = articles.map(article => ({
+  ...article,
+  itemProps: {
+    as: 'a',
+    href: 'https://google.com',
+  },
+}));
+
 storiesOf('ArticleList', module)
   .add('default', () => (
-    <ArticleList />
+    <ArticleList articles={articles} />
+  ))
+  .add('links', () => (
+    <ArticleList articles={articlesWithLink} />
   ));

bc8ab4925926b34c67b9a2782808986d8bf643ec
joylty94 <wowmen1757@gmail.com>
2018-02-06 18:01:14 +0900

    설정 추가
    gitignore, firebase 환경 변수

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import * as firebase from 'firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
-import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBqfBhUTQpeVLoBFi1obtknWjCwMPiuIWg',
-  authDomain: 'fds-redux-bbs-cf3e2.firebaseapp.com',
-  databaseURL: 'https://fds-redux-bbs-cf3e2.firebaseio.com',
-  projectId: 'fds-redux-bbs-cf3e2',
-  storageBucket: '',
-  messagingSenderId: '468057419445',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
 

28eab8c0787f75fdbcdb2c714151665479c4b2e3
joylty94 <wowmen1757@gmail.com>
2018-02-08 12:42:00 +0900

    firebase functoin 키 숨기기

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fetch = require('node-fetch');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// express 객체생셩
+const app = express()
+
+// 미들웨어 등록
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+
+const myToken = functions.config().github.token;
+
+
+app.get('/repos', (req, res) => {
+  return fetch('https://api.github.com/user/repos', { // Github에서 저장소 정보 가져오기, 노드버전이 6이라 비동기 then메소드릀 사용.
+    headers: {
+      Authorization: `token ${myToken}`
+    }
+  })
+  .then(res => res.json())
+  .then(data => {
+    res.send(data)
+  });
+})
+
+
+exports.github = functions.https.onRequest(app);
